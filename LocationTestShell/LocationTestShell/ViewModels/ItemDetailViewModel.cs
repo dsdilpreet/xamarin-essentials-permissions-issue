eb0ce4e9400f7615e5718b5b49f9cf4dabfcfa6a
Dilpreet Singh <dilpreet@compac.co.nz>
2020-01-20 10:09:07 +1300

    Add project files.

---
+++ LocationTestShell/LocationTestShell/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using LocationTestShell.Models;
+
+namespace LocationTestShell.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ LocationTestShell/LocationTestShell/ViewModels/LocationViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+namespace LocationTestShell.ViewModels
+{
+    class LocationViewModel : BaseViewModel
+    {
+
+        // Monitor location Task 
+        CancellationTokenSource monitorLocationTokenSource;
+        CancellationToken monitorLocationToken;
+        Task monitorLocationTask;
+
+
+        public LocationViewModel()
+        {
+            RequestLocationPermissionsCommand = new Command(async() => await RequestLocation());
+
+            StartTaskCommand = new Command(async() => await StartTask());
+
+            StartTaskCommand.Execute(null);
+        }
+
+        private async Task StartTask()
+        {
+            await StartMonitorLocationTask();
+        }
+
+        private async Task RequestLocation()
+        {
+            await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+            //MainThread.BeginInvokeOnMainThread(async() =>
+            //{
+            //    await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            //});
+        }
+
+        private async Task StartMonitorLocationTask()
+        {
+
+            monitorLocationTokenSource = new CancellationTokenSource();
+            monitorLocationToken = monitorLocationTokenSource.Token;
+
+            Action action = new Action(async () =>
+            {
+                while (!monitorLocationToken.IsCancellationRequested)
+                {
+
+                    if (!locationPermissionView)
+                    {
+                        Device.BeginInvokeOnMainThread(() => {
+                            DisplayLocationView();
+                        });
+                        Debug.WriteLine("Displaying Location View");
+                    }
+
+                    Debug.WriteLine($"Task Status: {monitorLocationTask.Status}");
+                    await Task.Delay(1);
+                }
+            });
+
+            monitorLocationTask = Task.Factory.StartNew(action, monitorLocationToken, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+            await Task.Delay(1);
+        }
+
+        private void DisplayLocationView()
+        {
+            LocationPermissionView = true;
+        }
+
+        private bool locationPermissionView;
+        public bool LocationPermissionView
+        {
+            get => locationPermissionView;
+            set
+            {
+                locationPermissionView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command RequestLocationPermissionsCommand { get; }
+        public Command StartTaskCommand { get; }
+    }
+}
